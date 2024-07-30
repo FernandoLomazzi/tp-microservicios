deb3be0009abcc5bcd2d5aad277b3b0fa5cef405
Lascot29 <fer.lomazzi29@gmail.com>
2024-07-29 21:13:30 -0300

    feat: implement data - service - controller

--- ms-productos/src/main/java/isi/dan/ms_productos/dto/StockUpdateDTO.java
+++ ms-productos/src/main/java/isi/dan/ms_productos/dto/StockUpdateDTO.java
@@ -1,9 +1,15 @@
 package isi.dan.ms_productos.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class StockUpdateDTO {
+	@NotNull(message = "id must not be null")
     private Long idProducto;
+    @Min(value = 0, message = "Cantidad debe ser mayor o igual a 0")
     private Integer cantidad;
+    @Min(value = 0, message = "Precio debe ser mayor o igual a 0")
+    private Double precio;
 }
